ef0789c2e6a98b9af4e30749129f579a8abe03c0
TB66 <1132315537@qq.com>
2020-08-15 15:23:05 +0800

    hello world

--- src/main/java/com/tb/springmvc/web/HelloAction.java
+++ src/main/java/com/tb/springmvc/web/HelloAction.java
@@ -1,4 +1,4 @@
-package com.tb.springmvc;
+package com.tb.springmvc.web;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
