97fff2244cb61872c2ae99265125204d500ed614
TB66 <1132315537@qq.com>
2020-08-15 15:01:00 +0800

    spring-mvc  HelloWorld

---
+++ src/main/java/com/tb/springmvc/HelloAction.java
@@ -0,0 +1,12 @@
+package com.tb.springmvc;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloAction {
+	@RequestMapping("hello")
+	public String hello() {
+		return "hello world";
+	}
+}
