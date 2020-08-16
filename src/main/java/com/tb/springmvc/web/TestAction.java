25e22df3dbec7dc950a9d6bbec28bf2b65394cd5
TB66 <1132315537@qq.com>
2020-08-16 16:06:54 +0800

    高级的参数注入

--- src/main/java/com/tb/springmvc/web/TestAction.java
+++ src/main/java/com/tb/springmvc/web/TestAction.java
@@ -1,6 +1,15 @@
 package com.tb.springmvc.web;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +35,19 @@ public class TestAction {
 	@GetMapping("**/get")
 	public String get() {
 		return "get";
+	}
+	
+	@RequestMapping("**/head")
+	public String head(@RequestHeader() String accept,@RequestHeader(name = "Connection",required = true)String conn) {
+		return accept+"<br>"+conn+"<br>";
+	}
+	
+	@RequestMapping("**/cookie")
+	public String cookie(@CookieValue String user,@CookieValue int age,@RequestHeader String cookie) {
+		return user+"<br>"+age+"<br>"+cookie;
+	}
+	@RequestMapping("**/servlet")
+	public String servlet(HttpServletRequest request,HttpServletResponse response,HttpSession session,InputStream in,OutputStream out) {
+		return request+"<br>"+response+"<br>"+session+"<br>"+in+"<br>"+out;
 	}
 }
