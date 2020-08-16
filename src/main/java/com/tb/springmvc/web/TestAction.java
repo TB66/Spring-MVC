42a7166b54bdea3eb4615621f0011dac76d07904
TB66 <1132315537@qq.com>
2020-08-16 16:07:53 +0800

    高级的参数注入

--- src/main/java/com/tb/springmvc/web/TestAction.java
+++ src/main/java/com/tb/springmvc/web/TestAction.java
@@ -20,34 +20,41 @@ public class TestAction {
 	public String add() {
 		return "add";
 	}
+
 	@GetMapping("del")
 	public String del() {
 		return "del";
 	}
+
 	@GetMapping("?/msg")
 	public String msg() {
 		return "msg";
 	}
+
 	@GetMapping("*/mod")
 	public String mod() {
 		return "mod";
 	}
+
 	@GetMapping("**/get")
 	public String get() {
 		return "get";
 	}
-	
+
 	@RequestMapping("**/head")
-	public String head(@RequestHeader() String accept,@RequestHeader(name = "Connection",required = true)String conn) {
-		return accept+"<br>"+conn+"<br>";
+	public String head(@RequestHeader() String accept,
+			@RequestHeader(name = "Connection", required = true) String conn) {
+		return accept + "<br>" + conn + "<br>";
 	}
-	
+
 	@RequestMapping("**/cookie")
-	public String cookie(@CookieValue String user,@CookieValue int age,@RequestHeader String cookie) {
-		return user+"<br>"+age+"<br>"+cookie;
+	public String cookie(@CookieValue String user, @CookieValue int age, @RequestHeader String cookie) {
+		return user + "<br>" + age + "<br>" + cookie;
 	}
+
 	@RequestMapping("**/servlet")
-	public String servlet(HttpServletRequest request,HttpServletResponse response,HttpSession session,InputStream in,OutputStream out) {
-		return request+"<br>"+response+"<br>"+session+"<br>"+in+"<br>"+out;
+	public String servlet(HttpServletRequest request, HttpServletResponse response, HttpSession session, InputStream in,
+			OutputStream out) {
+		return request + "<br>" + response + "<br>" + session + "<br>" + in + "<br>" + out;
 	}
 }
